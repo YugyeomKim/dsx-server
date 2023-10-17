d03d3d194c99d467d08a1e09f9fd329ea1a17a0e
YugyeomKim <hoodie0512@gmail.com>
2023-10-18 03:02:12 +0900

    run-model.py add model.eval() + torch.no_grad()

--- model/run-model.py
+++ model/run-model.py
@@ -7,15 +7,18 @@ from torchvision import transforms
 dirname = os.path.dirname(__file__)
 model = torch.jit.load(os.path.join(dirname, "alexnet_v1.pt"))
 
-image = Image.open(sys.argv[1])
+model.eval()
+with torch.no_grad():
+    image = Image.open(sys.argv[1])
 
-img_transforms = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Resize((227, 227), antialias=False),
-])
+    img_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((227, 227), antialias=False),
+    ])
 
-img_input = img_transforms(image)
-img_input = img_input.unsqueeze(0)
+    img_input = img_transforms(image)
+    img_input = img_input.unsqueeze(0)
 
-pred = model(img_input)
+    pred = model(img_input)
+
 print(pred.argmax().item())
